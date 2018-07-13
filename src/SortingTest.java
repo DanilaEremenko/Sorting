4d5afa75e184aa442bf911d7970ac6ca51a253b6
danil <danilaeremenko@mail.ru>
2018-07-14 01:36:07 +0300

    Сравнение с библиотечным деревом

--- src/SortingTest.java
+++ src/SortingTest.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.*;
 
 
 public class SortingTest {
-    private final int TEST_DIGIT = 10;
-    private final int ARRAYS_SIZE = 10;
-    private final int DIGITS_RADIUS = 10;
+    private final int TEST_DIGIT = 50;
+    private final int ARRAYS_SIZE = 10000;
+    private final int DIGITS_RADIUS = 10000;
 
     private Random random = new Random();
     private HashMap<Integer, ArrayList<Integer>> hashMap = generateHashMap();
@@ -20,21 +20,56 @@ public class SortingTest {
     private boolean MERGE_SORT = true;
     private boolean MERGE_FROM_INTERNET_SORT = true;
     private boolean QUICK_SORT = true;
+    private boolean TREE_SORT = true;
 
     private ArrayList<Integer> getRandomArrayList() {
         ArrayList<Integer> arrayList = new ArrayList<>();
-
         for (int i = 0; i < ARRAYS_SIZE; i++)
             arrayList.add(random.nextInt(DIGITS_RADIUS));
 
         return arrayList;
     }
 
+    private ArrayList<Integer> getRandomArrayListWithoutRepeat() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < ARRAYS_SIZE; i++) {
+            int digit;
+            do {
+                digit = random.nextInt(DIGITS_RADIUS);
+            }
+            while (arrayList.contains(digit));
+            arrayList.add(digit);
+        }
+
+        return arrayList;
+    }
+
+    private ArrayList<Integer> getUnsortedArraylist() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < ARRAYS_SIZE; i++)
+            arrayList.add(ARRAYS_SIZE - i);
+
+        return arrayList;
+    }
+
+    private ArrayList<Integer> getSortedArraylist() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < ARRAYS_SIZE; i++)
+            arrayList.add(i);
+
+        return arrayList;
+
+    }
+
+
     private HashMap<Integer, ArrayList<Integer>> generateHashMap() {
         HashMap<Integer, ArrayList<Integer>> hashMap = new HashMap<>();
 
         for (int i = 0; i < TEST_DIGIT; i++)
-            hashMap.put(i, getRandomArrayList());
+            hashMap.put(i, getUnsortedArraylist());
 
         return hashMap;
 
@@ -55,8 +90,6 @@ public class SortingTest {
 
 
     }
-
-
 
 
     @org.junit.Test
@@ -96,7 +129,7 @@ public class SortingTest {
     @Test
     public void consistsOfEqualsElements() {
         for (int i = 0; i < TEST_DIGIT; i++) {
-            int array1[] = listToArray(getRandomArrayList());
+            int array1[] = listToArray(getRandomArrayListWithoutRepeat());
             int array2[] = array1.clone();
             array2 = Sorting.arrayShuffle(array2);
             assertEquals(true,
@@ -105,8 +138,6 @@ public class SortingTest {
 
 
     }
-
-
 
 
     @org.junit.Test
@@ -188,6 +219,25 @@ public class SortingTest {
             }
             endTime = System.nanoTime();
             System.out.println("Quick Sort= " + (endTime - startTime) / normalize_koef);
+        }
+
+        if (TREE_SORT) {
+            TreeSet<Integer> treeSet = new TreeSet<>();
+            startTime = System.nanoTime();
+            for (int i = 0; i < TEST_DIGIT; i++) {
+                treeSet.addAll(hashMap.get(i));
+                int array[] = new int[treeSet.size()];
+                Iterator<Integer> iterator = treeSet.iterator();
+                for (int j = 0; j < treeSet.size(); j++)
+                    if (iterator.hasNext())
+                        array[j] = iterator.next();
+                    else break;
+                assertEquals(true,
+                        Sorting.arraysConsistsOfEqualsElements(array, listToArray(hashMap.get(i))));
+                assertEquals(true, Sorting.arrayIsSorted(array));
+            }
+            endTime = System.nanoTime();
+            System.out.println("Tree Sort= " + (endTime - startTime) / normalize_koef);
         }
 
 
