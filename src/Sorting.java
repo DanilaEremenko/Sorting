c2d834b14017cc4830cd47207e3859bd96940070
Danila Eremenko <danilaeremenko@mail.ru>
2018-08-22 00:32:02 +0300

    Все сортировки теперь работают с <T extends Comparable<? super T>>

--- src/Sorting.java
+++ src/Sorting.java
@@ -8,17 +8,16 @@ public class Sorting {
     //Worst and Average Case Time Complexity: O(n*n)
     //Auxiliary Space: O(1)
     //Stable: Yes
-    public static Integer[] bubbleSort(Integer array[]) {
-        Integer[] result;
-        result = array.clone();
+    public static <T extends Comparable<? super T>> List<T> bubbleSort(List<T> list) {
+        List<T> result = new ArrayList<>(list);
         while (true) {
             boolean end = true;
-            for (int i = 0; i < result.length - 1; i++) {
-                if (result[i] > result[i + 1]) {
-                    int digit;
-                    digit = result[i + 1];
-                    result[i + 1] = result[i];
-                    result[i] = digit;
+            for (int i = 0; i < result.size() - 1; i++) {
+                if (result.get(i).compareTo(result.get(i + 1)) > 0) {
+                    T digit;
+                    digit = result.get(i + 1);
+                    result.set(i + 1, result.get(i));
+                    result.set(i, digit);
                     end = false;
                 }
             }
@@ -30,15 +29,15 @@ public class Sorting {
 
     }
 
-    public static Integer[] bubbleSortFromInternet(Integer array[]) {
-        Integer[] result = array.clone();
-        for (int i = 0; i < result.length - 1; i++)
-            for (int j = 0; j < result.length - i - 1; j++)
-                if (result[j] > result[j + 1]) {
+    public static <T extends Comparable<? super T>> List<T> bubbleSortFromInternet(List<T> list) {
+        List<T> result = new ArrayList<>(list);
+        for (int i = 0; i < result.size() - 1; i++)
+            for (int j = 0; j < result.size() - i - 1; j++)
+                if (result.get(j).compareTo(result.get(j + 1)) > 0) {
                     // swap temp and arr[i]
-                    int temp = result[j];
-                    result[j] = result[j + 1];
-                    result[j + 1] = temp;
+                    T temp = result.get(j);
+                    result.set(j, result.get(j + 1));
+                    result.set(j + 1, temp);
                 }
         return result;
 
@@ -50,16 +49,16 @@ public class Sorting {
     //Worst and Average Case Time Complexity: O(n*n)
     //Auxiliary Space: O(1)
     //Stable: Yes
-    public static Integer[] insertionSort(Integer array[]) {
-        Integer result[] = array.clone();
-        for (int i = 1; i < result.length; ++i) {
-            int key = result[i];
+    public static <T extends Comparable<? super T>> List<T> insertionSort(List<T> list) {
+        List<T> result = new ArrayList<>(list);
+        for (int i = 1; i < result.size(); ++i) {
+            T key = result.get(i);
             int j = i - 1;
-            while (j >= 0 && result[j] > key) {
-                result[j + 1] = result[j];
+            while (j >= 0 && result.get(j).compareTo(key) > 0) {
+                result.set(j + 1, result.get(j));
                 j = j - 1;
             }
-            result[j + 1] = key;
+            result.set(j + 1, key);
         }
 
 
@@ -71,21 +70,22 @@ public class Sorting {
     //Worst and Average Case Time Complexity: O(n*logn)
     //Auxiliary Space: O(n)
     //Stable: Yes
-    private static Integer[] merge(Integer array1[], Integer array2[]) {
-        Integer result[] = new Integer[array1.length + array2.length];
-        int index1 = 0, index2 = 0;
-        for (int i = 0; i < result.length; i++) {
-            if (index1 == array1.length) {
-                result[i] = array2[index2];
+    private static <T extends Comparable<? super T>> List<T> merge(List<T> list1, List<T> list2) {
+        List<T> result = new ArrayList<>(list1.size() + list2.size());
+        int index1 = 0;
+        int index2 = 0;
+        for (int i = 0; i < result.size(); i++) {
+            if (index1 == list1.size()) {
+                result.set(i, list2.get(index2));
                 index2++;
-            } else if (index2 == array2.length) {
-                result[i] = array1[index1];
+            } else if (index2 == list2.size()) {
+                result.set(i, list1.get(index1));
                 index1++;
-            } else if (array1[index1] < array2[index2]) {
-                result[i] = array1[index1];
+            } else if (list1.get(index1).compareTo(list2.get(index2)) < 0) {
+                result.set(i, list1.get(index1));
                 index1++;
             } else {
-                result[i] = array2[index2];
+                result.set(i, list2.get(index2));
                 index2++;
             }
 
@@ -93,52 +93,59 @@ public class Sorting {
         return result;
     }
 
-    public static Integer[] mergeSort(Integer[] array) {
-        int mediana = array.length / 2;
-        Integer array1[] = Arrays.copyOfRange(array, 0, mediana);
-        Integer array2[] = Arrays.copyOfRange(array, mediana, array.length);
+    public static <T extends Comparable<? super T>> List<T> mergeSort(List<T> list) {
+        int mediana = list.size() / 2;
+        List<T> list1 = copyOfRange(list, 0, mediana);
+        List<T> list2 = copyOfRange(list, mediana, list.size());
         if (mediana > 0) {
-            array1 = mergeSort(array1);
-            array2 = mergeSort(array2);
+            list1 = mergeSort(list1);
+            list2 = mergeSort(list2);
         }
-        return merge(array1, array2);
+        return merge(list1, list2);
+    }
+
+    public static <T extends Comparable<? super T>> List<T> copyOfRange(List<T> list, int from, int to) {
+        List<T> result = new ArrayList<>();
+        for (int i = from; i < to; i++)
+            result.add(list.get(i));
+        return result;
     }
 
     //----------------------------------------------------------------------------------------------
 
-    private static Integer[] mergesortInner(Integer[] buffer1, Integer[] buffer2,
-                                            int startIndex, int endIndex) {
+    private static <T extends Comparable<? super T>> List<T> mergesortInner(
+            List<T> buffer1, List<T> buffer2, int startIndex, int endIndex) {
+
         if (startIndex >= endIndex - 1) {
             return buffer1;
         }
-
         // уже отсортирован.
         int middle = startIndex + (endIndex - startIndex) / 2;
-        Integer[] sorted1 = mergesortInner(buffer1, buffer2, startIndex, middle);
-        Integer[] sorted2 = mergesortInner(buffer1, buffer2, middle, endIndex);
+        List<T> sorted1 = mergesortInner(buffer1, buffer2, startIndex, middle);
+        List<T> sorted2 = mergesortInner(buffer1, buffer2, middle, endIndex);
 
         // Слияние
         int index1 = startIndex;
         int index2 = middle;
         int destIndex = startIndex;
-        Integer[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        List<T> result = sorted1 == buffer1 ? buffer2 : buffer1;
         while (index1 < middle && index2 < endIndex) {
-            result[destIndex++] = sorted1[index1] < sorted2[index2]
-                    ? sorted1[index1++] : sorted2[index2++];
+            result.set(destIndex++, sorted1.get(index1).compareTo(sorted2.get(index2)) < 0
+                    ? sorted1.get(index1++) : sorted2.get(index2++));
         }
         while (index1 < middle) {
-            result[destIndex++] = sorted1[index1++];
+            result.set(destIndex++, sorted1.get(index1++));
         }
         while (index2 < endIndex) {
-            result[destIndex++] = sorted2[index2++];
+            result.set(destIndex++, sorted2.get(index2++));
         }
         return result;
     }
 
-    public static Integer[] mergesortFromInternet(Integer[] array1) {
-        Integer[] buffer1 = Arrays.copyOf(array1, array1.length);
-        Integer[] buffer2 = new Integer[array1.length];
-        return mergesortInner(buffer1, buffer2, 0, array1.length);
+    public static <T extends Comparable<? super T>> List<T> mergesortFromInternet(List<T> list) {
+        List<T> buffer1 = copyOfRange(list, 0, list.size());
+        List<T> buffer2 = new ArrayList<>(list.size());
+        return mergesortInner(buffer1, buffer2, 0, list.size());
     }
 
 
@@ -148,46 +155,45 @@ public class Sorting {
     //Worst:    O(n*n)
     //Auxiliary Space: O(n)
     //Stable: No
-    private static int partition(Integer array[], int low, int high) {
-        int pivot = array[high];
+    private static <T extends Comparable<? super T>> int partition(List<T> list, int low, int high) {
+        T pivot = list.get(high);
         int i = (low - 1);
         for (int j = low; j < high; j++) {
-            if (array[j] <= pivot) {
+            if (list.get(j).compareTo(pivot) <= 0) {
                 i++;
 
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                T temp = list.get(i);
+                list.set(i, list.get(j));
+                list.set(j, temp);
             }
         }
 
-        int temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
+        T temp = list.get(i + 1);
+        list.set(i + 1, list.get(high));
+        list.set(high, temp);
 
         return i + 1;
     }
 
 
-    private static void quickSort(Integer[] array, int low, int high) {
+    private static <T extends Comparable<? super T>> void quickSort(List<T> list, int low, int high) {
         if (low < high) {
 
-            int pi = partition(array, low, high);
+            int pi = partition(list, low, high);
 
 
-            quickSort(array, low, pi - 1);
-            quickSort(array, pi + 1, high);
+            quickSort(list, low, pi - 1);
+            quickSort(list, pi + 1, high);
         }
     }
 
-    public static Integer[] quickSort(Integer array[]) {
-        Integer result[] = array.clone();
+    public static <T extends Comparable<? super T>> List<T> quickSort(List<T> list) {
+        List<T> result = new ArrayList<>(list);
 
-
-        int pi = partition(result, 0, result.length - 1);
+        int pi = partition(result, 0, result.size() - 1);
 
         quickSort(result, 0, pi - 1);
-        quickSort(result, pi + 1, result.length - 1);
+        quickSort(result, pi + 1, result.size() - 1);
 
         return result;
     }
@@ -199,61 +205,54 @@ public class Sorting {
     //Worst:    O(n*logn)
     //Auxiliary Space: O(n)
     //Stable: Yes
-    public static Integer[] standardTreeSort(Integer array[]) {
-        Integer result[] = array.clone();
-        TreeSet<Integer> treeSet = new TreeSet<>(Arrays.asList(result));
+    public static <T extends Comparable<? super T>> List<T> standardTreeSort(List<T> list) {
+        List<T> result = new ArrayList<T>(list);
+        TreeSet<T> treeSet = new TreeSet<>(result);
 
-        Iterator<Integer> iterator = treeSet.iterator();
+        Iterator<T> iterator = treeSet.iterator();
         int i = 0;
         while (iterator.hasNext()) {
-            result[i] = iterator.next();
+            result.set(i, iterator.next());
             i++;
         }
         return result;
     }
 
-    public static Integer[] myTreeSort(Integer array[]) {
-        Integer result[] = array.clone();
-        BinaryTree<Integer> binaryTree = new BinaryTree<>();
-        for (int anResult : result)
-            binaryTree.add(anResult);
-
-        Iterator<Integer> iterator = binaryTree.iterator();
-        int i = 0;
-        while (iterator.hasNext()) {
-            result[i] = iterator.next();
-            i++;
-        }
-        return result;
-
-    }
+//    public static <T extends Comparable<? super T>> List<T> myTreeSort(List<T> list) {
+//        List<T> result = new ArrayList<>(list);
+//        BinaryTree<T> binaryTree = new BinaryTree<>();
+//        for (T anResult : result)
+//            binaryTree.add(T);
+//
+//        Iterator<T> iterator = binaryTree.iterator();
+//        int i = 0;
+//        while (iterator.hasNext()) {
+//            result.set(i, iterator.next());
+//            i++;
+//        }
+//        return result;
+//
+//    }
 
     //----------------------------------------------------------------------------------------------
 
     //Different Methods for arrays
-    public static boolean arrayIsSorted(Integer array[]) {
-        for (int i = 0; i < array.length - 1; i++)
-            if (array[i] > array[i + 1])
+    public static <T extends Comparable<? super T>> boolean listIsSorted(List<T> list) {
+        for (int i = 0; i < list.size() - 1; i++)
+            if (list.get(i).compareTo(list.get(i + 1)) > 0)
                 return false;
         return true;
     }
 
 
-    private static boolean contains(Integer array[], int digit) {
-        for (int anArray : array)
-            if (anArray == digit)
-                return true;
-        return false;
-    }
-
-
-    public static boolean arraysConsistsOfEqualsElements(Integer array1[], Integer array2[]) {
-        if (array1.length != array2.length)
+    public static <T extends Comparable<? super T>> boolean listsConsistsOfEqualElements
+            (List<T> list1, List<T> list2) {
+        if (list1.size() != list2.size())
             return false;
 
-        List<Integer> list = new ArrayList<>(Arrays.asList(array2));
+        List<T> list = new ArrayList<>(list2);
 
-        for (int anArray1 : array1) {
+        for (T anArray1 : list1) {
             boolean hasFound = false;
             for (int i = 0; i < list.size(); i++)
                 if (list.get(i) == anArray1) {
@@ -265,47 +264,6 @@ public class Sorting {
                 return false;
         }
         return true;
-
-    }
-
-
-    public static Integer[] arrayShuffle(Integer array[]) {
-        Integer randomedIndexes[] = new Integer[array.length];
-        int rIndex = 0;
-        Integer result[] = new Integer[array.length];
-
-        result[0] = array[0];
-
-        for (int i = 1; i < array.length; i++) {
-            int j;
-            do
-                j = random.nextInt(array.length);
-            while (Sorting.contains(randomedIndexes, j));
-            result[i] = array[j];
-            randomedIndexes[rIndex] = j;
-            rIndex++;
-        }
-        return result;
-    }
-
-
-    public static void printArray(Integer array[]) {
-        for (int anArray : array)
-            System.out.println(anArray);
-        System.out.println("\n");
-    }
-
-
-    public static void printTwoArray(Integer array1[], Integer array2[]) {
-        if (array1.length != array2.length) {
-            System.out.println("Неравный размер массивов");
-            return;
-        }
-
-        System.out.println("FirstArray\tSecondArray");
-        for (int i = 0; i < array1.length; i++)
-            System.out.println(array1[i] + "\t\t\t" + array2[i]);
-
 
     }
 
